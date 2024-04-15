94e729744fd0492cbb661a985195623766b8fcdb
Angela-CMU <chienyul@andrew.cmu.edu>
2024-04-14 23:36:11 -0400

    Complete single variable distribution plot

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ def load_data():
 
 def plot_feature_distribution(X):
   ##### selectbox #####
-  feature_names = ['age', 'education', 'martial-status', 'race', 'sex', 'native-country']
+  feature_names = ['age', 'education', 'marital-status', 'race', 'sex', 'native-country']
   label_name = ['income']
   feature_select = st.selectbox('Feature distribution', feature_names)
 
@@ -29,10 +29,12 @@ def plot_feature_distribution(X):
     X_feature = X[feature_select]
     bin = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     fig = plt.figure()
-    plt.hist(X_feature, bins=bin, color='skyblue', edgecolor='black')
-    plt.xlabel('Age')
+    plt.hist(X_feature, bins='auto', color='skyblue', edgecolor='black')
+    plt.xlabel(feature_select)
     plt.ylabel('Frequency')
-    plt.title('Age Distribution')
+    plt.title(f'{feature_select} Distribution')
+    plt.xticks(rotation=90, fontsize=8)
+    plt.tight_layout()
     st.pyplot(fig)
 
 def main():
