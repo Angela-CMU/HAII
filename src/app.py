d480f577d048703e1ff5d8bbde5086313f9f0f06
Angela-CMU <chienyul@andrew.cmu.edu>
2024-04-14 23:55:39 -0400

    add 2nd plot

--- src/app.py
+++ src/app.py
@@ -37,11 +37,36 @@ def plot_feature_distribution(X):
     plt.tight_layout()
     st.pyplot(fig)
 
+def plot_two_feature_distribution(X):
+    feature_names = ['age', 'education', 'marital-status', 'race', 'sex']
+    feature1 = st.selectbox('Select First Feature', feature_names)
+    feature2 = st.selectbox('Select Second Feature', feature_names)
+    if feature1 and feature2:
+        fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+
+        # Plot distribution of first selected feature
+        axes[0].hist(X[feature1], bins='auto', color='skyblue', edgecolor='black')
+        axes[0].set_xlabel(feature1)
+        axes[0].set_ylabel('Frequency')
+        axes[0].set_title(f'{feature1} Distribution')
+        axes[0].tick_params(axis='x', rotation=90, labelsize=8)
+
+        # Plot distribution of second selected feature
+        axes[1].hist(X[feature2], bins='auto', color='salmon', edgecolor='black')
+        axes[1].set_xlabel(feature2)
+        axes[1].set_ylabel('Frequency')
+        axes[1].set_title(f'{feature2} Distribution')
+        axes[1].tick_params(axis='x', rotation=90, labelsize=8)
+
+        plt.tight_layout()
+        st.pyplot(fig)
+
 def main():
   X, y = load_data()
   st.dataframe(X.head())
 
   plot_feature_distribution(X)
+  plot_two_feature_distribution(X)
 
 if __name__ == '__main__':
   main()
