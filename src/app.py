78e93f27cf3fc8c6571c52d9dc9cd908319e1b01
Yen-Ju Wu <yenjuw@andrew.cmu.edu>
2024-04-14 22:55:55 -0400

    show training datasets in streamlit

--- src/app.py
+++ src/app.py
@@ -1,3 +1,25 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
+import numpy as np
+from ucimlrepo import fetch_ucirepo 
 
-st.write("Hello world")
+st.header("FairPay: Empowering Fairness in Interactive Income Analysis")
+st.write("Team members: Yen-Ju Wu (yenjuw@andrew.cmu.edu) and Chien-Yu Liu (chienyul@andrew.cmu.edu)")
+
+@st.cache_data
+def load_data():
+  # fetch dataset 
+  adult = fetch_ucirepo(id=2) 
+    
+  # data (as pandas dataframes) 
+  X = adult.data.features 
+  y = adult.data.targets 
+  return X, y
+
+def main():
+  X, y = load_data()
+  st.dataframe(X.head())
+
+if __name__ == '__main__':
+  main()
